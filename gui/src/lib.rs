d98434f33e0390dfd95ad0d0aa0c2ed1e429fc92
loaden <loaden@live.com>
2023-09-16 12:54:18 +0800

    Make binary crate as thin as possible

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,6 +1,17 @@
-pub fn greet(name: &str) -> String {
+pub fn create_app<R: tauri::Runtime>(
+    builder: tauri::Builder<R>,
+) -> tauri::App<R> {
+    builder
+        .invoke_handler(tauri::generate_handler![greet])
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
     if name.len() == 0 || name.trim().len() == 0 {
-        return "Please input your name!".to_owned()
+        return "Please input your name!".to_string();
     }
     format!(
         "Hello, {}! You've been greeted from {} {}!",
--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,12 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod cmd;
-use cmd::*;
-
 fn main() {
-    println!("{}", gitauri::times(5, 9));
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    gitauri::create_app(tauri::Builder::default()).run(|_app_handle, event| {
+        match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        }
+    });
 }
