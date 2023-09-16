1d26b0fcc82efd2bdc2413e569af9bccfd7d698b
loaden <loaden@live.com>
2023-09-16 10:46:04 +0800

    Make the binary crate thin enough

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,3 +1,12 @@
+pub fn greet(name: &str) -> String {
+    format!(
+        "Hello, {}! You've been greeted from {} {}!",
+        name,
+        base::G_APPNAME,
+        base::add(1, 2) + times(2, 3)
+    )
+}
+
 pub fn times(left: usize, right: usize) -> usize {
     left * right
 }
--- gui/src/main.rs
+++ gui/src/main.rs
@@ -3,10 +3,8 @@
 mod cmd;
 use cmd::*;
 
-use gitauri::times;
-
 fn main() {
-    println!("{}", times(5, 9));
+    println!("{}", gitauri::times(5, 9));
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
